2e6a49dd603724cd1331f9165f9bbd8192f7451a
anjalipb-369 <33362511+anjalipb-369@users.noreply.github.com>
2018-10-11 10:32:49 +0000

    Add files via upload

---
+++ LoginPage.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="VSM.LoginPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-xs-1 col-sm-1 col-md-3"></div>
+        <div class="col-xs-10  col-sm-10 col-md-6">
+            <asp:Label ID="lblMobileNo" style="text-align:left;" class="control-label col-sm-12 col-md-5 col-xs-12" runat="server" Text="Mobile No :"></asp:Label>
+        &nbsp;&nbsp;
+            <asp:TextBox ID="txtBoxMobileNo" class="col-xs-12 col-sm-12 col-md-5"  runat="server"></asp:TextBox>
+        </div>
+        <div class="col-xs-1  col-md-3"></div>
+    </div>
+    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+        ControlToValidate="txtBoxMobileNo" ErrorMessage="*" ForeColor="Red"></asp:RequiredFieldValidator>
+    <br />    
+    <br />
+    <div class="row">
+        <div class="col-xs-1 col-sm-1 col-md-3"></div>
+        <div class="col-xs-10 col-sm-10 col-md-6">
+            <asp:Label ID="lblPswd"  style="text-align:left;" class="control-label col-sm-12 col-md-5 col-xs-12" runat="server" Text="Password :"></asp:Label>
+        &nbsp;&nbsp;
+            <asp:TextBox ID="txtBoxPswd"  class="col-xs-12 col-sm-12 col-md-5" runat="server" TextMode="Password"></asp:TextBox>
+        </div>
+        <div class="col-xs-1  col-md-3"></div>
+    </div>
+    <br />
+<br />
+    <div class="row col-xs-12 col-sm-12 col-md-12">
+        <div class="col-xs-4 col-sm-5 col-md-5"></div>
+        <asp:Button ID="btnLogin" runat="server"  class="col-xs-6 col-sm-2 col-md-2 btn btn-primary"  onclick="btnLogin_Click" 
+    Text="Login" />
+        <div class="col-xs-3 col-sm-5  col-md-5"></div>
+    </div>
+<br />
+<br />
+</asp:Content>
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace VSM
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+        SqlDataReader dataRead;
+        SqlCommand command;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtBoxMobileNo.Text == "9999999999" && txtBoxPswd.Text == "admin")
+                Response.Redirect("Upload_database.aspx");
+            else
+            {
+                command = new SqlCommand("SELECT * FROM usertable where mobileno = @mobileno AND password = @password", con);
+                command.Parameters.AddWithValue("@mobileno", txtBoxMobileNo.Text);
+                command.Parameters.AddWithValue("@password", txtBoxPswd.Text);
+                dataRead = command.ExecuteReader();
+                if (dataRead.Read())
+                {
+                    Session["id"] = dataRead["userid"];
+                    Session["name"] = dataRead["name"];
+                    if (Convert.ToString(dataRead["usertype"]).Equals("V"))
+                        Response.Redirect("VolunteerProfile.aspx");
+                    else
+                        Response.Redirect("EmployeeProfile.aspx");
+                }
+                else
+                {
+                    string script = "window.onload = function(){ alert('No such record found.');";
+                    script += "window.location = 'LoginPage.aspx';};";
+                    ClientScript.RegisterStartupScript(this.GetType(), "SuccessMessage", script, true);
+                }
+            }
+        }
+    }
+}
