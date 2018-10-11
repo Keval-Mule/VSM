24c27875fbef9dc2088c16d4bcc0c269fa4532c5
Keval-Mule <mulekeval@gmail.com>
2018-10-11 10:53:42 +0530

    Add files via upload

---
+++ VolunteerProfile.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="VolunteerProfile.aspx.cs" Inherits="VSM.VolunteerProfile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container-fluid jumbotron">
+        <asp:Label ID="lblVolunteerName" class="" runat="server" Text="Volunteer Name"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btnEditProfile" class="btn btn-success" runat="server" Text="Edit Profile" 
+            onclick="btnEditProfile_Click" />
+        <br />
+        <br />
+        <asp:Button ID="btnUpdatePrvsEvent" class="btn btn-success" runat="server" Text="Rate Event" 
+            onclick="btnUpdatePrvsEvent_Click" />
+        <br />
+        <br />
+    </div>
+</asp:Content>
---
+++ VolunteerProfile.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace VSM
+{
+    public partial class VolunteerProfile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+        SqlDataReader dataRead;
+        SqlCommand command;
+        string id;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["name"] != null)
+            {
+                lblVolunteerName.Text = Session["name"].ToString();
+                id = Convert.ToString(Session["id"]);
+            }
+            else
+                Response.Redirect("index.html");
+            Master.FindControl("btnLogout").Visible = true;
+            //con.Open();
+            //id = Convert.ToString(Session["id"]);
+            //command = new SqlCommand("SELECT name FROM usertable where userid = @userid", con);
+            //command.Parameters.AddWithValue("userid", Convert.ToInt32(id));
+            //dataRead = command.ExecuteReader();
+            //if (dataRead.Read())
+            //    lblVolunteerName.Text = Convert.ToString(dataRead["name"]);
+        }
+
+        protected void btnEditProfile_Click(object sender, EventArgs e)
+        {
+            string name = lblVolunteerName.Text.Trim();
+            Session["name"] = name;
+            Session["id"] = id;
+            Response.Redirect("EditVolunteerPage.aspx");
+        }
+
+        protected void btnUpdatePrvsEvent_Click(object sender, EventArgs e)
+        {
+            string name = lblVolunteerName.Text.Trim();
+            Session["name"] = name;
+            Response.Redirect("ViewNRateEvent.aspx");
+        }
+    }
+}
