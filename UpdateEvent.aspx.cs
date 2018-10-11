ddcfffe6e452558b2032096ad0d7a0353495c772
HrishikeshThombare <hrishikeshthombare@gmail.com>
2018-10-11 10:33:50 +0000

    Add files via upload

---
+++ UpdateEvent.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.IO;
+
+namespace VSM
+{
+    public partial class UpdateEvent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["name"] != null)
+            {
+                lblEmpName.Text = Session["name"].ToString();
+            }
+            else
+                Response.Redirect("index.html");
+            Master.FindControl("btnLogout").Visible = true;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            Response.Redirect("EmployeeProfile.aspx");
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Verifies that the control is rendered */
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Charset = "";
+            string FileName = "CustomerDetails" + DateTime.Now + ".xls";
+            StringWriter strwritter = new StringWriter();
+            HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+            GridView1.GridLines = GridLines.Both;
+            GridView1.HeaderStyle.Font.Bold = true;
+            GridView1.RenderControl(htmltextwrtter);
+            Response.Write(strwritter.ToString());
+            Response.End();  
+        }
+    }
+}
