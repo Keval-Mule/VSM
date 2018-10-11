2dd36ea026692faaaabeb099184c272c5270d2bd
Puja Nagpure <puja30895@gmail.com>
2018-10-11 10:03:06 +0000

    Add files via upload

---
+++ EmployeeProfile.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="EmployeeProfile.aspx.cs" Inherits="VSM.EmployeeProfile" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="col-sm-12 col-xs-12">
+        <div class="col-sm-3 col-xs-3"></div>
+        <div class="col-sm-6 col-xs-6">
+            <asp:Label ID="lblEmpName" class="col-sm-12 col-xs-12" runat="server" Text="Employee Name"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnEditProfile" class="btn btn-primary col-sm-12 col-xs-12" runat="server" Text="Edit Profile" 
+                onclick="btnEditProfile_Click" TabIndex="10" />
+            <br />
+            <br />
+            <asp:Button ID="btnUpdate" class="btn btn-primary col-sm-12 col-xs-12" runat="server" onclick="btnUpdate_Click" 
+                Text="Update/Delete event" />
+            <br />
+            <br />
+            <asp:Button ID="btnAddNewEvent" class="btn btn-primary col-sm-12 col-xs-12" runat="server" Text="Add New Event" 
+                onclick="btnAddNewEvent_Click" />
+            <br />
+            <br />
+            <asp:Button ID="Button1" class="btn btn-primary col-sm-12 col-xs-12" runat="server" Text="View Volunteer Record" 
+                onclick="Button1_Click" />
+            <br />
+        </div>
+        <div class="col-sm-3 col-xs-3"></div>
+    </div>
+</asp:Content>
---
+++ EmployeeProfile.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VSM
+{
+    public partial class EmployeeProfile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["name"] != null)
+            {
+                lblEmpName.Text = Session["name"].ToString();
+            }
+            else
+                Response.Redirect("index.html");
+            Master.FindControl("btnLogout").Visible = true;
+        }
+
+        protected void btnEditProfile_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            this.Session["id"] = Session["id"];
+            Response.Redirect("EditEmployeeProfile.aspx");
+        }
+
+        protected void btnAddNewEvent_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            this.Session["id"] = Session["id"];
+            Response.Redirect("AddEvent.aspx");
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            this.Session["id"] = Session["id"];
+            Response.Redirect("UpdateEvent.aspx");
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            Response.Redirect("VolunteerRecord.aspx");
+        }
+    }
+}
