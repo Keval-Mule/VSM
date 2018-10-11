0fbf70af0408896f648701b05cdf690fa9d3f0ae
HrishikeshThombare <hrishikeshthombare@gmail.com>
2018-10-11 10:34:54 +0000

    Add files via upload

---
+++ ViewNRateEvent.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="ViewNRateEvent.aspx.cs" Inherits="VSM.ViewNRateEvent" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <asp:Label ID="lblEmpName" runat="server" Text="Label"></asp:Label>
+            <cc1:toolkitscriptmanager ID="ToolkitScriptManager1" runat="server" />
+        </div>
+        <div class="panel-body">
+            <asp:Button ID="Button1" runat="server" CssClass="btn btn-success" 
+                onclick="Button1_Click" Text="Go Back" />
+            <br />
+            <asp:GridView ID="gvFruits" CssClass="panel-body" runat="server" 
+                AutoGenerateColumns="false" DataKeyNames="id"
+                HeaderStyle-BackColor="#3AC0F2" HeaderStyle-ForeColor="White" RowStyle-BackColor="#A1DCF2"
+                AlternatingRowStyle-BackColor="White" AlternatingRowStyle-ForeColor="#000" AllowSorting="True">
+                <Columns>
+                    <asp:BoundField DataField="EventName" HeaderText="Event Name" />
+                    <asp:TemplateField HeaderText="Ratings">
+                        <ItemTemplate>
+                            <cc1:Rating ID="Rating1" AutoPostBack="true" OnChanged="OnRatingChanged" runat="server"
+                                StarCssClass="Star" WaitingStarCssClass="WaitingStar" EmptyStarCssClass="Star"
+                                FilledStarCssClass="FilledStar" CurrentRating='<%# Eval("Rating") %>'>
+                            </cc1:Rating>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </div>
+    </div>
+</asp:Content>
---
+++ ViewNRateEvent.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using AjaxControlToolkit;
+
+namespace VSM
+{
+    public partial class ViewNRateEvent : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString);
+        string id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["name"] != null)
+            {
+                lblEmpName.Text = Session["name"].ToString();
+                id = Convert.ToString(Session["id"]);
+            }
+            else
+                Response.Redirect("index.html");
+            Master.FindControl("btnLogout").Visible = true;
+
+            if (!IsPostBack)
+            {
+                gvFruits.DataSource = GetData("SELECT id, EventName, ISNULL((SELECT AVG(Rating) FROM EventRating WHERE id = EventInfo.id), 0) Rating FROM EventInfo");
+                gvFruits.DataBind();
+            }
+        }
+        private static DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            string constr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+
+        protected void OnRatingChanged(object sender, RatingEventArgs e)
+        {
+            int rowIndex = ((sender as Rating).NamingContainer as GridViewRow).RowIndex;
+            int fruitId = Convert.ToInt32(gvFruits.DataKeys[rowIndex].Value);
+            string constr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO EventRating VALUES(@id, @Rating)"))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.AddWithValue("@id", fruitId);
+                    cmd.Parameters.AddWithValue("@Rating", e.Value);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = lblEmpName.Text.Trim();
+            Session["name"] = name;
+            Response.Redirect("VolunteerProfile.aspx");
+        }
+
+    }
+}
